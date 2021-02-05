b812ae4b45687f3ddfcc2defb56868c0f6cc5e1f
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-05 15:54:07 +0200

    Add files via upload

---
+++ thu_python_16/program1.py
@@ -0,0 +1,11 @@
+print('Окей гугл')
+print(10)
+print(10+10)
+print('10'+'10')
+print('10+10')
+print('Привет ' + 'Григорий')
+
+while True:
+	print('Vzlom//')
+	print('...')
+
---
+++ thu_python_16/program2.py
@@ -0,0 +1,18 @@
+#name=input('Введите кол-во ')
+#print('Баланс: '+name+'$')
+
+print(100/10)
+print(10-100)
+print(10*10)
+print(2**3)
+
+print((2+2)*2)
+
+num1 = input('Введите число 1: ')
+num2 = input('Введите число 2: ')
+
+print(int(num1)+int(num2))
+
+#holodilnic=10
+#xleb=15
+#print('10+15=',holodilnic+xleb)
