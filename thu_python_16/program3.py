b0d9c924efd277662fc786f09b8949e93e769c8e
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-11 20:11:22 +0200

    Add files via upload

---
+++ thu_python_16/program3.py
@@ -0,0 +1,42 @@
+#x=10
+#print(x)
+
+#y=10
+#print(x+y)
+
+#print('x+y')
+
+#x=20
+#z = 10
+#print(x+y+z)
+
+#x='Яготинское'
+#k='молоко'
+#print(x+' '+k)
+#print('Яготинское'+' молоко')
+
+#print(k * 143543543)
+
+#print(11 > 10)
+#print(8 > 9)
+#print(9 != 9)
+#print(9 == 9)
+
+#x = 8
+#y = 9
+#print(x >= y)
+
+#print('a' == 'a')
+#print('с' == 'c')
+
+
+#print('z' > 'a')
+
+password = input('Введите пароль: ')
+
+if 'abc123' == password:
+	print('Вход выполнен')
+else:
+	print('Невход выполнен 404')
+
+
