202c76c5c3619df3c41760c88cdee7502bd1356c
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-11 20:06:40 +0200

    Add files via upload

---
+++ thu_python_16/paint_turtle3.py
@@ -0,0 +1,20 @@
+import turtle
+
+t=turtle.Pen()
+#--------------------
+t.shape('turtle')
+
+
+colors = ['black', 'yellow', 'lime', 'blue', 'magenta']
+print(colors)
+print(colors[1])
+
+t.pencolor(colors[4])
+t.left(68)
+t.forward(100)
+
+
+
+#--------------------
+turtle.done()
+
