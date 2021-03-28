f515ef36f4138b74e36cea080236f496e671b78c
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-28 20:09:11 +0300

    Add files via upload

---
+++ thu_python_16/robot.py
@@ -0,0 +1,15 @@
+name_player = input('Введите имя: ')
+name_bot = 'BOT123'
+print(name_bot + ' -> приветсвует конг-фу мастера ' + name_player)
+
+message = input('Введите фразу: ')
+if message == 'по чем рис?':
+    print(name_bot + ' -> дорого, 1300 uh/kg')
+elif message == 'сколько тебе лет?':
+    print(name_bot + ' -> 100010110 лет')
+    
+    
+    
+    
+else:
+    print(name_bot + ' -> error 404. Введите другую фразу ')
